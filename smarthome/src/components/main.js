c2e917e8271855109fd1937c3af89f5602e432e8
santiagocala <s.cala@uniandes.edu.co>
2021-12-03 13:44:41 -0500

    Clase

---
+++ smarthome/src/components/main.js
@@ -0,0 +1,12 @@
+import Post from './post';
+
+function Main(){
+    return(
+        <div>
+            <h1>Listado de Posts</h1>
+            <Post/>
+        </div>
+    )
+}
+
+export default Main;
